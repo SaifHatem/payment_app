386d9098f5665d3154bd199229641995e71d4b82
Seif Hatem <shatem793@gmail.com>
2024-12-01 01:47:26 +0200

    create paypal models

--- lib/features/checkout/presentation/widgets/custom_buttom_bloc_consumer.dart
+++ lib/features/checkout/presentation/widgets/custom_buttom_bloc_consumer.dart
@@ -5,6 +5,7 @@ import 'package:final_pay/features/checkout/presentation/manager/cubit/payment_c
 import 'package:final_pay/features/checkout/presentation/views/thankyou_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 import '../../../../core/utils/widgets/cutsom_button.dart';
 
@@ -36,13 +37,80 @@ class CustomButtomBlockConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
           onTap: () {
-            PaymentIntentInputModel paymentIntentInputModel =
-                PaymentIntentInputModel(
-                    amount: 1000,
-                    currency: 'USD',
-                    customerId: 'cus_RJbWQrSaoXEkdV');
-            BlocProvider.of<PaymentCubit>(context)
-                .makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            // PaymentIntentInputModel paymentIntentInputModel =
+            //     PaymentIntentInputModel(
+            //         amount: 1000,
+            //         currency: 'USD',
+            //         customerId: 'cus_RJbWQrSaoXEkdV');
+            // BlocProvider.of<PaymentCubit>(context)
+            //     .makePayment(paymentIntentInputModel: paymentIntentInputModel);
+
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => PaypalCheckoutView(
+                sandboxMode: true,
+                clientId: "YOUR CLIENT ID",
+                secretKey: "YOUR SECRET KEY",
+                transactions: const [
+                  {
+                    "amount": {
+                      "total": '100',
+                      "currency": "USD",
+                      "details": {
+                        "subtotal": '100',
+                        "shipping": '0',
+                        "shipping_discount": 0
+                      }
+                    },
+                    "description": "The payment transaction description.",
+                    // "payment_options": {
+                    //   "allowed_payment_method":
+                    //       "INSTANT_FUNDING_SOURCE"
+                    // },
+                    "item_list": {
+                      "items": [
+                        {
+                          "name": "Apple",
+                          "quantity": 4,
+                          "price": '10',
+                          "currency": "USD"
+                        },
+                        {
+                          "name": "Pineapple",
+                          "quantity": 5,
+                          "price": '12',
+                          "currency": "USD"
+                        }
+                      ],
+
+                      // Optional
+                      //   "shipping_address": {
+                      //     "recipient_name": "Tharwat samy",
+                      //     "line1": "tharwat",
+                      //     "line2": "",
+                      //     "city": "tharwat",
+                      //     "country_code": "EG",
+                      //     "postal_code": "25025",
+                      //     "phone": "+00000000",
+                      //     "state": "ALex"
+                      //  },
+                    }
+                  }
+                ],
+                note: "Contact us for any questions on your order.",
+                onSuccess: (Map params) async {
+                  log("onSuccess: $params");
+                  Navigator.pop(context);
+                },
+                onError: (error) {
+                  log("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                  Navigator.pop(context);
+                },
+              ),
+            ));
           },
           isLoading: state is PaymentLoading ? true : false,
           buttonText: 'Continue',
